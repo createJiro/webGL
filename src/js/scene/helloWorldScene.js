8f59c15d7dc34c4cc9f8f917b62653fde5b7a94b
森下　健一 <ken_morishita@galboa.co.jp>
2019-02-21 20:19:48 +0900

    タッチ判定テスト処理

--- src/js/scene/helloWorldScene.js
+++ src/js/scene/helloWorldScene.js
@@ -1,5 +1,20 @@
 export default class HelloWorldScene extends Phaser.Scene {
     create() {
-      this.add.text(250, 150, 'Hello World', { fill: '#fff' });
+
+      // 文字表示
+      // text(位置x,位置y,中身,設定)
+      // setOrigin : x・yの詰め (1,1)だと右詰め・上詰め
+      // setInteractive : タッチイベント用
+      var hello = this.add.text(250, 150, 'Hello World', { fill: '#fff' }).setOrigin(0.5).setInteractive();
+
+      var hello2 = this.add.text(250, 150, '', { fill: '#fff' });
+
+      // タッチ判定（離れた際）
+      hello.on('pointerup', function(pointer, localX, localY, event){
+        
+        // 文字表示
+        hello2.setText("Hello World2");
+
+      })
     }
 }
